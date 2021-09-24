223e8ac02ae9044c8b8df733267a84f4295c13bc
Pratham Jha <prathamjha5683@gmail.com>
2021-09-25 00:39:28 +0530

    Html carousel pending

--- frontend/src/Components/content-body.js
+++ frontend/src/Components/content-body.js
@@ -1,21 +1,26 @@
 import React from "react";
-
+import { Navbar, Nav, Container, Row, Col, Card, Button, Image } from 'react-bootstrap'
+import Mission from "./Images/content-mission.svg";
+import Community from "./Images/helping-about.svg";
+import "../Pages/HomeScreen.css";
 function Content() {
     return (
-        <div>
-            <div className="introduction">
-                <h1 className="introduction-heading">Welcome to Helping Jet</h1>
-                <p className="introduction-body">This Website is built for the sole purpose of Helping the Elder People for their Medical,Financial and Social Support.</p>
-            </div>
+        <Container className="fluid">
             <div className="Our Mission">
-                <h1>Our Mission</h1>
-                <p>Our Mission is to make a safer Environment for the living of Elder People where we provide help to them by Connecting them to different particular NGO's for their welfare and we also provide 50% of our Donation amount to the needy Elders  </p>
+                <div className="Mission-content">
+                <h1>Our Mission</h1> 
+                <Image className = " " src= {Mission} width ="300px" height="300px"/>
+                <p>Our Mission is to make a safer Environment for the living of Elder People where we provide help to them by Connecting them to different particular NGO's for their welfare and we also provide 50% of our Donation amount to the needy Elders.  </p>
+                 </div>
             </div>
-            <div className="About-us">
-                    <h1>About Us</h1>
-                    <p></p>
+            <div className="Community">
+                    <h1>Community</h1>
+                    <div className="Community-content">
+                    <Image className = "" src ={Community} width = "300px" height = "300px"/>    
+                    <p>We are a Growing Community of large amount of people and other Communities who consents to our Mission and Ideology for the Welfare of the Elderly People </p>
+                    </div>
             </div>
-        </div>
+        </Container>
     );
 }
 export default Content;

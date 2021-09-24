1bb25c55aa4f1cb1fb0711f2683616ecab818058
bishalpandit <bishalpandit17@gmail.com>
2021-09-24 18:37:14 +0530

    Finished header section 1.0

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,22 +1,41 @@
 import React from 'react'
-import { NavLink, Navbar, Nav, Container } from 'react-bootstrap'
+import HelpingOld1 from './Images/helping-old-1.svg'
+import HelpingOld2 from './Images/helping-old-2.svg'
+import logo from './Images/logo.png'
+import { Navbar, Nav, Container, Row, Col, Card, Button, Image } from 'react-bootstrap'
+import '../Pages/HomeScreen.css';
 export default function Header() {
     return (
-        <div>
+        <div className="header">
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">Helping Jet</Navbar.Brand>
+                    <Navbar.Brand href="#home"><img src={logo} height="80px" width="80px"/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
+                        <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link1</Nav.Link>
-                            <Nav.Link href="#link">Link2</Nav.Link>
-                            <Nav.Link href="#link">Link3</Nav.Link>
+                            <Nav.Link href="#link">Volunteer</Nav.Link>
+                            <Nav.Link href="#link">Partner</Nav.Link>
+                        </Nav>
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#link"><Button className="rounded-pill btn-lg btn-info light">Donate</Button></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+
+            <Container>
+                
+                <Row>
+                <h1 className="text-center mt-5 ">Helping Needy Elderly for their Welfare and Empowering them.</h1>
+                    <Col>
+                        <Image className="header-img" src={HelpingOld1} height="500px" width="500px" />
+                    </Col>
+                    <Col>
+                        <Image className="header-img" src={HelpingOld2} height="500px" width="500px" />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
--- frontend/src/Pages/HomeScreen.js
+++ frontend/src/Pages/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Components/Header';
+import './HomeScreen.css';
 
 export default function HomeScreen() {
     return (
